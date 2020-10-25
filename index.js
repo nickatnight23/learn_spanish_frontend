2f09709083550523e32f87bb5bdb98281df0cb8d
Latricia <tricianickelberry@gmail.com>
2020-10-25 13:26:37 -0700

    post fetch request

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const endPoint = "http://localhost:3000/api/v1/topics"
 
 document.addEventListener('DOMContentLoaded', () => {
      getTopics()
-})
+
+     const createTopicForm = document.querySelector("#create-topic-form")
+
+     createTopicForm.addEventListener("submit",(e) => 
+     createFormHandler(e))
+    })
+
 
 function getTopics() {
     fetch(endPoint)
@@ -21,4 +27,39 @@ function getTopics() {
        }) 
     
     })
-}
+}       
+
+        function createFormHandler(e){
+            e.preventDefault()
+            const titleInput = document.querySelector('#input-title').value
+            const contentInput = document.querySelector('#input-content').value
+            const categoryInput = document.querySelector('#categories').value
+            const categoryId = parseInt(categoryInput)
+            postFetch(titleInput, contentInput,  categoryInput)
+        }
+
+        function postFetch(title, content, category_id){
+                console.log(title, content, category_id);
+                const bodyData ={title, content, category_id}
+                fetch(endPoint, {
+                    // POST request
+                    method: "POST",
+                    headers: {"Content-Type": "application/json"},
+                    body: JSON.stringify(bodyData)
+                  })
+                  .then(response => response.json())
+                  .then(topic => {
+                    const topicData = topic.data.attributes
+                    // render JSON response
+                    const topicMarkup = `
+                    <div data-id=${topic.id}>
+                      <h3>${topicData.title}</h3>
+                      <p>${topicData.category.name}</p>
+                      <button data-id=${topicData.id}>edit</button>
+                    </div>
+                    <br><br>`;
+
+                    document.querySelector('#topic-container').innerHtml += topicMarkup;
+
+        })
+    }
